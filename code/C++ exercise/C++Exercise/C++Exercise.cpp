8884fddd5cda269e3c021feaac6636e8445c1d8d
holis-yang <blueyang@sjtu.edu.cn>
2020-02-06 10:37:16 +0800

    initiate repository

---
+++ code/C++ exercise/C++Exercise/C++Exercise.cpp
@@ -0,0 +1,69 @@
+﻿// C++Exercise.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool g_InvalidInput = false;
+
+double PowerWithUnsignedExponent(double base, unsigned exponent)
+{
+	if (exponent == 0)
+		return 0;
+	if (exponent == 1)
+		return base;
+
+	double result = PowerWithUnsignedExponent(base, exponent >> 1);
+	result *= result;
+	if ((exponent & 0x1) == 1)
+		return result *= base;
+
+	return result;
+}
+
+double Power(double base, int exponent)
+{
+	g_InvalidInput = false;
+
+	if ((base == 0.0) && exponent < 0)
+	{
+		g_InvalidInput = true;
+		return 0.0;
+	}
+
+	unsigned int absExponent = (unsigned int)(exponent);
+	if (exponent < 0)
+		absExponent = (unsigned int)(-exponent);
+
+	double result = PowerWithUnsignedExponent(base, absExponent);
+	if (exponent < 0)
+		result = 1.0 / result;
+	return result;
+}
+int main()
+
+{
+	//double result = Power(2, 6);
+	//cout << result << endl;
+	int a = 5;
+	int& rb = a;
+	
+
+	return 0;
+}
+
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
